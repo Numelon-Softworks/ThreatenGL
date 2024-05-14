6ee5057c32d97c7a4b5fab4e116e6fb9ed37c40e
Richy-Z <64844585+Richy-Z@users.noreply.github.com>
2024-05-14 18:49:35 +0100

    Update fabric implementation, restructure repository

--- fabric/src/main/java/com/numelon/threatengl/fabric/client/ThreatenGLFabricClient.java
+++ fabric/src/main/java/com/numelon/threatengl/fabric/client/ThreatenGLFabricClient.java
@@ -1,10 +1,10 @@
-package com.numelon.threatengl.fabric.client;
+/*package com.numelon.threatengl.fabric.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public final class ExampleModFabricClient implements ClientModInitializer {
+public final class ThreatenGLFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
     }
-}
+}*/
