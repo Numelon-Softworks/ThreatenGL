7002302310b7494d507c04759f763218980007d6
Richy-Z <64844585+Richy-Z@users.noreply.github.com>
2024-05-14 17:59:24 +0100

    Shakespeare

--- common/src/main/java/com/numelon/threatengl/ExampleMod.java
+++ common/src/main/java/com/numelon/threatengl/ExampleMod.java
@@ -1,9 +1,16 @@
 package com.numelon.threatengl;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public final class ExampleMod {
     public static final String MOD_ID = "threatengl";
 
+    public static final Logger LOGGER = LogManager.getLogger("ThreatenGL");
+
     public static void init() {
         // Write common init code here.
+        LOGGER.info("bomboclart");
+        LOGGER.info("Hark! This mod doth seem to toil as expected! O, dear sire, rejoice! Muahaha!");
     }
 }
