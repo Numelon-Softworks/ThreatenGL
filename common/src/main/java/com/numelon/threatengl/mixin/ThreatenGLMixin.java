ee3dacf913c32fbb47e7cb22c24b03fad94e5f51
Richy Z <64844585+Richy-Z@users.noreply.github.com>
2024-05-18 23:09:37 +0100

    macOS OpenGL 4.1 and semantic version number

--- common/src/main/java/com/numelon/threatengl/mixin/ThreatenGLMixin.java
+++ common/src/main/java/com/numelon/threatengl/mixin/ThreatenGLMixin.java
@@ -17,12 +17,25 @@ public class ThreatenGLMixin {
 
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwWindowHint(II)V", remap = false))
     private void hintOverride(int hint, int value) {
+        // check if on macOS
+        String osName = System.getProperty("os.name").toLowerCase();
+
         // Checking whether the window hint intercepted is the GLFW Context Version
         if (hint == GLFW.GLFW_CONTEXT_VERSION_MAJOR) {
             value = 4;
             LOGGER.info("Threatened Minecraft to use GLFW_CONTEXT_VERSION_MAJOR " + value);
         } else if (hint == GLFW.GLFW_CONTEXT_VERSION_MINOR) {
-            value = 6;
+            // NOTE: I am aware that macOS uses OpenGL 4.1 by default with Minecraft. This was only added so that Minecraft initialises instead of complaining about the OpenGL version, for users who are unaware of the state of OpenGL on macOS.
+            
+            // OpenGL stopped receiving support since OS X Mavericks (10.9) in favour of metal of course :heart_eyes:
+            // if on macOS, use 4.1 instead of 4.6
+            if (osName.contains("mac")) {
+                LOGGER.info("It looks like you're on macOS, which ditched OpenGL in 2013 in favour of Metal. The maximum supported version is 4.1, so that will be used instead of 4.6.");
+                value = 1;
+            } else {
+                // all other OSes
+                value = 6;
+            }
             LOGGER.info("Threatened Minecraft to use GLFW_CONTEXT_VERSION_MINOR " + value);
         }
 
